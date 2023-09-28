9d2f700ace941b3a892f82221cccff8fdd7f4ebf
aannnssss <st4ro5tinaa@yandex.ru>
2023-09-29 01:40:57 +0300

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/annastarostina/Downloads/programming/lab1_oop
+# Build directory: /Users/annastarostina/Downloads/programming/lab1_oop/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BASIC "/Users/annastarostina/Downloads/programming/lab1_oop/build/tests")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/Users/annastarostina/Downloads/programming/lab1_oop/CMakeLists.txt;16;add_test;/Users/annastarostina/Downloads/programming/lab1_oop/CMakeLists.txt;0;")
